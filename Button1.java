52715e2bedca1ae2941a95ff002f79d7d5ce9a7f
James Jardine <jamesjardine@gmx.com>
2019-09-16 13:39:27 +0100

    additional text area

---
+++ Button1.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Button1 {
+
+    public static void main(String[] args) {
+        Button1 gui = new Button1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me lots and lots and lots!");
+        Font bigFont = new Font("Tahoma", Font.BOLD, 28);
+        button.setFont(bigFont);
+        frame.getContentPane().add(BorderLayout.NORTH, button);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
